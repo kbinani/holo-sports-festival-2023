2b62ae928404a928d59685422600f95623bf40dc
kbinani <kbinani.bt@gmail.com>
2023-10-23 03:52:23 +0900

    Init

---
+++ src/main/java/com/github/kbinani/holosportsfestival2023/Main.java
@@ -0,0 +1,7 @@
+package com.github.kbinani.holosportsfestival2023;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+}
