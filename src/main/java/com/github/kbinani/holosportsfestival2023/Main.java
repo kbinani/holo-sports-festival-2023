3c1111376e7fa1e736bb11c95aeb0808b7e52877
kbinani <kbinani.bt@gmail.com>
2023-10-23 04:11:58 +0900

    mob の自然湧きを禁止

--- src/main/java/com/github/kbinani/holosportsfestival2023/Main.java
+++ src/main/java/com/github/kbinani/holosportsfestival2023/Main.java
@@ -1,7 +1,93 @@
 package com.github.kbinani.holosportsfestival2023;
 
+import org.bukkit.Difficulty;
+import org.bukkit.GameRule;
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Level;
+
 public class Main extends JavaPlugin implements Listener {
+  private World world;
+
+  @Override
+  public void onEnable() {
+    Optional<World> overworld = getServer().getWorlds().stream().filter(it -> it.getEnvironment() == World.Environment.NORMAL).findFirst();
+    if (overworld.isEmpty()) {
+      getLogger().log(Level.SEVERE, "server should have at least one overworld dimension");
+      setEnabled(false);
+      return;
+    }
+    world = overworld.get();
+
+    List<String> reasons = new ArrayList<>();
+    List<String> warnings = new ArrayList<>();
+    Boolean mobGriefing = world.getGameRuleValue(GameRule.MOB_GRIEFING);
+    Boolean keepInventory = world.getGameRuleValue(GameRule.KEEP_INVENTORY);
+    Boolean showDeathMessages = world.getGameRuleValue(GameRule.SHOW_DEATH_MESSAGES);
+    Boolean announceAdvancements = world.getGameRuleValue(GameRule.ANNOUNCE_ADVANCEMENTS);
+    if (mobGriefing != null && mobGriefing) {
+      reasons.add("mobGriefing gamerule is set to true");
+    }
+    if (keepInventory != null && !keepInventory) {
+      reasons.add("keepInventory gamerule is set to false");
+    }
+    if (showDeathMessages != null && showDeathMessages) {
+      warnings.add("showDeathMessages gamerule is set to true");
+    }
+    if (announceAdvancements != null && announceAdvancements) {
+      warnings.add("announceAdvancements gamerule is set to true");
+    }
+    if (world.getDifficulty() == Difficulty.PEACEFUL) {
+      reasons.add("the \"himerace\" mini game is not playable as the difficulty is set to peaceful");
+    }
+    if (!world.getPVP()) {
+      reasons.add("pvp is set to false");
+    }
+    if (!reasons.isEmpty()) {
+      getLogger().log(Level.SEVERE, "Disabling the plugin because:");
+      for (String reason : reasons) {
+        getLogger().log(Level.SEVERE, "  " + reason);
+      }
+      setEnabled(false);
+      return;
+    }
+    if (!warnings.isEmpty()) {
+      for (String warning : warnings) {
+        getLogger().warning(warning);
+      }
+    }
+
+    PluginManager pluginManager = getServer().getPluginManager();
+    pluginManager.registerEvents(this, this);
+  }
+
+  @EventHandler
+  @SuppressWarnings("unused")
+  public void onCreatureSpawn(CreatureSpawnEvent e) {
+    if (e.isCancelled()) {
+      return;
+    }
+    if (e.getEntity().getWorld() != world) {
+      return;
+    }
+    switch (e.getSpawnReason()) {
+      case NATURAL:
+      case VILLAGE_INVASION:
+      case BUILD_WITHER:
+      case BUILD_IRONGOLEM:
+      case BUILD_SNOWMAN:
+      case SPAWNER_EGG:
+      case SPAWNER:
+        e.setCancelled(true);
+        break;
+    }
+  }
 }
