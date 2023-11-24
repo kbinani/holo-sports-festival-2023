965b86edf5d03c89ba65e4afe2eda9be6de576ed
kbinani <kbinani.bt@gmail.com>
2023-11-24 10:48:50 +0900

    [plugin][Himerace] 調理器具の案内表示を出す

--- plugin/src/main/java/com/github/kbinani/holosportsfestival2023/himerace/CookStage.java
+++ plugin/src/main/java/com/github/kbinani/holosportsfestival2023/himerace/CookStage.java
@@ -1,13 +1,16 @@
 package  com.github.kbinani.holosportsfestival2023.himerace;
 
+import com.github.kbinani.holosportsfestival2023.Kill;
 import com.github.kbinani.holosportsfestival2023.Point3i;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.World;
+import org.bukkit.entity.TextDisplay;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.joml.Matrix4f;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -28,6 +31,7 @@ class CookStage extends AbstractStage {
   @Override
   protected void onStart() {
     setFinished(true);
+    prepare();
   }
 
   @Override
@@ -40,6 +44,7 @@ class CookStage extends AbstractStage {
 
   @Override
   protected void onReset() {
+    Kill.EntitiesByScoreboardTag(world, Stage.COOK.tag);
   }
 
   @Override
@@ -72,5 +77,67 @@ class CookStage extends AbstractStage {
         yield Component.empty();
       }
     };
+  }
+
+  private void prepare() {
+    world.spawn(pos(-97, 82, 11).toLocation(world).add(0, -0.34, -0.02), TextDisplay.class, it -> {
+      it.text(Component.text("鉄板").color(NamedTextColor.GREEN));
+      it.addScoreboardTag(Stage.COOK.tag);
+      it.setTransformationMatrix(new Matrix4f().rotateY((float) (Math.PI)));
+    });
+    world.spawn(pos(-97, 82, 11).toLocation(world).add(0, -0.5, -0.02), TextDisplay.class, it -> {
+      it.text(Component.text("Hot Plate").color(NamedTextColor.GREEN));
+      it.addScoreboardTag(Stage.COOK.tag);
+      it.setTransformationMatrix(new Matrix4f().rotateY((float) (Math.PI)).scale(0.6f));
+    });
+
+    world.spawn(pos(-98, 82, 9).toLocation(world).add(0.02, -0.34, 0.5), TextDisplay.class, it -> {
+      it.text(Component.text("鍋").color(NamedTextColor.GREEN));
+      it.addScoreboardTag(Stage.COOK.tag);
+      it.setTransformationMatrix(new Matrix4f().rotateY((float) (90.0 / 180.0 * Math.PI)));
+    });
+    world.spawn(pos(-98, 82, 9).toLocation(world).add(0.02, -0.5, 0.5), TextDisplay.class, it -> {
+      it.text(Component.text("Cauldron").color(NamedTextColor.GREEN));
+      it.addScoreboardTag(Stage.COOK.tag);
+      it.setTransformationMatrix(new Matrix4f().rotateY((float) (90.0 / 180.0 * Math.PI)).scale(0.6f));
+    });
+
+    world.spawn(pos(-98, 82, 8).toLocation(world).add(0.02, -0.34, 0.5), TextDisplay.class, it -> {
+      it.text(Component.text("盛り付け台").color(NamedTextColor.GREEN));
+      it.addScoreboardTag(Stage.COOK.tag);
+      it.setTransformationMatrix(new Matrix4f().rotateY((float) (90.0 / 180.0 * Math.PI)));
+    });
+    world.spawn(pos(-98, 82, 8).toLocation(world).add(0.02, -0.5, 0.5), TextDisplay.class, it -> {
+      it.text(Component.text("Serving Table").color(NamedTextColor.GREEN));
+      it.addScoreboardTag(Stage.COOK.tag);
+      it.setTransformationMatrix(new Matrix4f().rotateY((float) (90.0 / 180.0 * Math.PI)).scale(0.6f));
+    });
+
+    world.spawn(pos(-97, 82, 7).toLocation(world).add(0, -0.34, 0.02), TextDisplay.class, it -> {
+      it.text(Component.text("まな板").color(NamedTextColor.GREEN));
+      it.addScoreboardTag(Stage.COOK.tag);
+    });
+    world.spawn(pos(-97, 82, 7).toLocation(world).add(0, -0.5, 0.02), TextDisplay.class, it -> {
+      it.text(Component.text("Cutting Board").color(NamedTextColor.GREEN));
+      it.addScoreboardTag(Stage.COOK.tag);
+      it.setTransformationMatrix(new Matrix4f().scale(0.6f));
+    });
+  }
+
+  private int x(int x) {
+    return x + 100 + origin.x;
+  }
+
+  private int y(int y) {
+    return y - 80 + origin.y;
+  }
+
+  private int z(int z) {
+    return z - 1 + origin.z;
+  }
+
+  private Point3i pos(int x, int y, int z) {
+    // [-100, 80, 1] は赤チーム用 Level の origin
+    return new Point3i(x(x), y(y), z(z));
   }
 }
